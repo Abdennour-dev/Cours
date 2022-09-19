ca5e6ed4477b526acb47d3a8ae865b4b5c6e9ff3
Abdennour-dev <88547770+Abdennour-dev@users.noreply.github.com>
2022-09-19 23:50:52 +0100

    Add files via upload

--- react/react.js
+++ react/react.js
@@ -30,7 +30,7 @@ react:
 - npm -v     npm لمعرفة اصدار
 
 - npx create-react-app my-app                                           كود التسطيب
-  cd my-app                                                           عند انتهاء التسطيب ادخل على ملف التطبيق
+  cd my-app                                                           عند انتهاء التطيب ادخل على ملف التطبيق
   npm start                               react   ويظهر شعار ال localhost:3000     بعدها مباشرة ياخذنا على   npm ونشغل ال
 
                        هذا دليل على نجاح عملية التسطيب                                                                 
@@ -53,4 +53,109 @@ react:
 - index.js على 
 
 - html شبيهة قليلا بال Jsx اكواد ال 
+
+################################################################
+
+                                         INTRODUCTION :
+                                         -------------
+
+app.js code :
+--------------
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+
+  const myname = "abdennor";                               نركز هنا في هذه العبارة
+  const element = <h1>Hello, {myname}</h1>;
+
+  return (
+    <div className="App">
+
+      <header className="App-header">{element}</header>    هنا ايضا 
+
+    </div>
+  );
+}
+
+export default App;
+
+-------------------------------------------------------
+             functions  التعامل مع ال 
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+const element = (
+  <h1>
+    Hello, {formatName(user)}!
+  </h1>
+);
+
+------------------------------------------------------
+                                if condition التعامل مع ال 
+                            
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+
+--------------------------------------------------------------
+                    
+                         attributes   الروابط والصور 
+
+const element = <a href="https://www.reactjs.org"> link </a>;
+
+const element = <img src={user.avatarUrl}></img>;
+
+--------------------------------------------------------------------
+ 
+ يجب احترام نظام الاب والابن  Jsx ملاحظة في نظام 
+ 
+ < >
+         <=======   او استخدام 
+ </>
+
+----------------------------------------------------------------------
+
+Babel compiles JSX down to React.createElement() calls.
+
+These two examples are identical:
+
+const element = (
+  <h1 className="greeting">
+    Hello, world!
+  </h1>
+);
+
+
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello, world!'
+);
+
+
+// Note: this structure is simplified
+
+const element = {
+  type: 'h1',
+  props: {
+    className: 'greeting',
+    children: 'Hello, world!'
+  }
+};
+
+
+################################################################
+
+
 */
